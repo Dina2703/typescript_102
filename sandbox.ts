19b027e15717c0761d9b01aa2800d4e71282ec56
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-03-24 22:11:03 -0400

    #2 Compiling Typescript

---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+//run command: tsc sandbox.ts -w
+
+const character = "mario";
+
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+
+inputs.forEach((input) => {
+  console.log(input);
+});
