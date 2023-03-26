e6568afafd3f65a9fdcd84345660fe702f088dd0
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-03-25 21:27:00 -0400

    Typescript: Type Basics

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,15 @@
-var character = "mario";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+//run command: tsc sandbox.ts -w
+// const character = "mario";
+// console.log(character);
+// const inputs = document.querySelectorAll("input");
+// console.log(inputs);
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+// ------------#3 Type Basics---------------
+var age = 30;
+age = "hi";
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(10));
--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,24 @@
 //run command: tsc sandbox.ts -w
 
-const character = "mario";
+// const character = "mario";
 
-console.log(character);
+// console.log(character);
 
-const inputs = document.querySelectorAll("input");
+// const inputs = document.querySelectorAll("input");
 
-console.log(inputs);
+// console.log(inputs);
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+
+// ------------#3 Type Basics---------------
+
+let age = 30;
+age = "hi";
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(10));
