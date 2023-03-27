0df890fdd0a54ecfcaddd572703083f353efe772
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-03-26 23:30:50 -0400

    #9 Type Aliases

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -93,26 +93,39 @@
 // console.log("test");
 
 // ------------#8 Function Basics---------------
-let greet: Function;
+// let greet: Function;
 
-greet = () => {
-  console.log("hello, World");
-};
+// greet = () => {
+//   console.log("hello, World");
+// };
 //for 'c' the  default value is 10, if we don't pass any other value.
-const add = (a: number, b: number, c: number | string = 10) => {
-  console.log(a + b + c);
-  console.log(c);
-};
+// const add = (a: number, b: number, c: number | string = 10) => {
+//   console.log(a + b + c);
+//   console.log(c);
+// };
 
-add(2, 4);
-add(2, 4, 33);
+// add(2, 4);
+// add(2, 4, 33);
 
 //example with a function that returns a value
-const minus = (a: number, b: number): number => {
-  return a + b;
-};
+// const minus = (a: number, b: number): number => {
+//   return a + b;
+// };
 
-let result = minus(10, 7);
-console.log(typeof result);
+// let result = minus(10, 7);
+// console.log(typeof result);
 
 //void when a function returns nothing
+
+// ------------#9 Type Aliases---------------
+type StringOrNum = string | number;
+type objWithName = { name: string; uid: StringOrNum };
+
+const logDetails = {uid: StringOrNum, item: string} => {
+  console.log(`${item} has a uid of ${uid}`);
+}
+
+const greet = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
+  
+}
